56c007c0af084a0ac0661fb5a8790740004da9c4
Onis brown <onis brown>
2020-05-13 19:23:49 +0100

    added custom if for testing

--- blocks/LindLogic.js
+++ blocks/LindLogic.js
@@ -38,6 +38,30 @@ var listCondXJSON ={
   "tooltip": "choose your conditions"
 };
 
+var lindIfJSON ={
+    "type": "controls_if",
+    "message0": "%{BKY_CONTROLS_IF_MSG_IF} %1",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "IF0",
+      }
+    ],
+    "message1": "%{BKY_CONTROLS_IF_MSG_THEN} %1",
+    "args1": [
+      {
+        "type": "input_statement",
+        "name": "DO0"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "style": "logic_blocks",
+    "helpUrl": "%{BKY_CONTROLS_IF_HELPURL}",
+    "mutator": "controls_if_mutator",
+    "extensions": ["controls_if_tooltip"]
+}
+
 Blockly.Blocks['listCond'] = {
   init: function() {
     this.jsonInit(listCondXJSON);
--- generators/javascript/LindLogic.js
+++ generators/javascript/LindLogic.js
@@ -7,3 +7,38 @@ Blockly.JavaScript['listCond'] = function(block) {
   var code = `${target} ${comp} ${lim}`;
   [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
+
+Blockly.javascript['lindIf'] = function(block){
+  // If/elseif/else condition.
+  var n = 0;
+  var code = '', branchCode, conditionCode;
+  if (Blockly.JavaScript.STATEMENT_PREFIX) {
+    // Automatic prefix insertion is switched off for this block.  Add manually.
+    code += Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_PREFIX,
+        block);
+  }
+  do {
+    conditionCode = Blockly.JavaScript.valueToCode(block, 'IF' + n,
+        Blockly.JavaScript.ORDER_NONE) || 'false';
+    branchCode = Blockly.JavaScript.statementToCode(block, 'DO' + n);
+    if (Blockly.JavaScript.STATEMENT_SUFFIX) {
+      branchCode = Blockly.JavaScript.prefixLines(
+          Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_SUFFIX,
+          block), Blockly.JavaScript.INDENT) + branchCode;
+    }
+    code += (n > 0 ? ' else ' : '') +
+        'if (' + conditionCode + ') {\n' + branchCode + '}';
+    ++n;
+  } while (block.getInput('IF' + n));
+
+  if (block.getInput('ELSE') || Blockly.JavaScript.STATEMENT_SUFFIX) {
+    branchCode = Blockly.JavaScript.statementToCode(block, 'ELSE');
+    if (Blockly.JavaScript.STATEMENT_SUFFIX) {
+      branchCode = Blockly.JavaScript.prefixLines(
+          Blockly.JavaScript.injectId(Blockly.JavaScript.STATEMENT_SUFFIX,
+          block), Blockly.JavaScript.INDENT) + branchCode;
+    }
+    code += ' else {\n' + branchCode + '}';
+  }
+    return code + '\n';
+};
